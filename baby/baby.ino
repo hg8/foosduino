2303e0ccd104579bde9c5edee7917215e291bf09
nozkrew <thomas.maire1@hotmail.fr>
2017-01-31 11:02:03 +0100

    Big ass refactor

--- baby/baby.ino
+++ baby/baby.ino
@@ -1,11 +1,14 @@
 #include <Wire.h>
 #include "rgb_lcd.h"
 
-#define PIR_MOTION_SENSOR_1 10
+#define PIR_MOTION_SENSOR_1 12
 #define PIR_MOTION_SENSOR_2 2
 #define MELODYPIN 4
-#define REDGAMELLEBUTTON 8
+#define REDGAMELLEBUTTON 10
 #define BLUEGAMELLEBUTTON 6
+
+//Gamelle rouge a le bouton poussoire
+//Gamelle bleue a le bouton tactile
 
 #define NOTE_C4  262
 #define NOTE_D4  294
@@ -39,8 +42,8 @@ rgb_lcd lcd;
 int redGamelleButtonState = 0;  
 int blueGamelleButtonState = 0;
 
-int scoreRed = 0;
-int scoreBlue = 0;
+int scoreRed;
+int scoreBlue;
 
 
 void setup()
@@ -50,24 +53,14 @@ void setup()
 
     pinMode(REDGAMELLEBUTTON, INPUT);
     pinMode(BLUEGAMELLEBUTTON, INPUT);
-    
-
-    lcd.begin(16, 2);
-    lcd.noBlinkLED();
-    lcd.clear();
-    lcd.setRGB(0, 255, 0);
-    lcd.print("Get ready !");
-    delay(3000);
-    lcd.clear();
-    lcd.print("Rouge / Bleu ");
-    lcd.setCursor(0, 1);
-    lcd.print(String(scoreRed));
-    lcd.setCursor(11, 1);
-    lcd.print(String(scoreBlue));
 
     pinMode(MELODYPIN, OUTPUT);
 
+    //Init score + écran
+    initialisation();
 
+    //Afichage du score 
+    displayScore();
 }
 
 void loop()
@@ -78,102 +71,88 @@ void loop()
 
     //Button gamelle
     if (redGamelleButtonState == HIGH) {
-        scoreRed = scoreRed - 1;
-        lcd.clear();
-        lcd.print("Gamelle !");
-        delay(1000);
-        lcd.clear();
-        lcd.print("Rouge / Bleu ");
-        lcd.setCursor(0, 1);
-        lcd.print(String(scoreRed));
-        lcd.setCursor(11, 1);
-        lcd.print(String(scoreBlue));
-        delay(2000);
+        scoreRed = gamelle(scoreRed);
         
+        displayScore();
     }
 
     if(blueGamelleButtonState==1){
+        //TODO : voir si nécessaire
         blueGamelleButtonState = 0;
-        scoreBlue = scoreBlue - 1;
-        lcd.clear();
-        lcd.print("Gamelle !");
-        delay(1000);
-        lcd.clear();
-        lcd.print("Rouge / Bleu ");
-        lcd.setCursor(0, 1);
-        lcd.print(String(scoreRed));
-        lcd.setCursor(11, 1);
-        lcd.print(String(scoreBlue));
-        delay(2000);
+
+        scoreBlue = gamelle(scoreBlue);
+        
+        displayScore();
     }
 
   
     if(isGoalDetected(PIR_MOTION_SENSOR_1)){
-        lcd.setRGB(255, 0, 0);
+        lcd.setRGB(248, 16, 16);
         scoreRed++;
-
         
-        lcd.clear();
-        lcd.print("Rouge / Bleu ");
-        lcd.setCursor(0, 1);
-        lcd.print(String(scoreRed));
-        lcd.setCursor(11, 1);
-        lcd.print(String(scoreBlue));
+        displayScore();
 
-        goal();
+        goalMelody();
 
-        if(scoreRed == 10){
-          lcd.clear();
-          lcd.setRGB(255, 0, 0);
-          lcd.print("Rouge gagne!");
-          lcd.blinkLED();
-
-          win();
-          
-          delay(100000);
-        }
-
-        delay(5000);
-
-        lcd.setRGB(155, 155, 155);
-
-        
+        hasWin(scoreRed, "Rouge");
     }
 
-       if(isGoalDetected(PIR_MOTION_SENSOR_2)){
-        lcd.setRGB(0, 0, 255);
+    if(isGoalDetected(PIR_MOTION_SENSOR_2)){
+        lcd.setRGB(42, 56, 211);
         scoreBlue++;
 
-        lcd.clear();
-        lcd.print("Rouge / Bleu ");
-        lcd.setCursor(0, 1);
-        lcd.print(String(scoreRed));
-        lcd.setCursor(11, 1);
-        lcd.print(String(scoreBlue));
+        displayScore();
 
-        goal();
+        goalMelody();
 
-        if(scoreBlue == 10){
-          lcd.clear();
-          lcd.setRGB(0, 0, 255);
-          lcd.print("Bleu gagne!");
-          lcd.blinkLED();
-
-          win();
-          
-          delay(100000);
-        }
+        hasWin(scoreBlue, "Bleu");
         
-        delay(5000);
-
-        lcd.setRGB(155, 155, 155);
     }
+
+    delay(5000);
+    lcd.setRGB(155, 155, 155);
 
 }
 
+void hasWin(int score, String team){
+    if(score == 10){
+        lcd.clear();
+        if(team == "Rouge"){
+          lcd.setRGB(248, 16, 16);
+        }
+        else{
+          lcd.setRGB(42, 56, 211);
+        }
+        
+        lcd.print(team+ " gagne!");
+        lcd.blinkLED();
 
-boolean isGoalDetected(int SENSOR)
-{
+        winMelody();
+        delay(20000);
+        
+        setup();
+    }
+}
+
+int gamelle(int score){
+    score = score - 1;
+    lcd.clear();
+    lcd.print("Gamelle !");
+    delay(1000);
+    return score;
+}
+
+void displayScore(){
+    lcd.clear();
+    lcd.print("Rouge / Bleu ");
+    lcd.setCursor(0, 1);
+    lcd.print(String(scoreRed));
+    lcd.setCursor(11, 1);
+    lcd.print(String(scoreBlue));
+}
+
+boolean isGoalDetected(int SENSOR){
+  
     int sensorValue = digitalRead(SENSOR);
     if(sensorValue == HIGH)
     {
@@ -203,8 +182,22 @@ void buzz(int targetPin, long frequency, long length) {
 
 }
 
+void initialisation(){
+
+    scoreBlue = 0;
+    scoreRed = 0;
+    
+    //Initialisation de l'écran
+    lcd.begin(16, 2);
+    lcd.noBlinkLED();
+    lcd.clear();
+    lcd.setRGB(0, 255, 0);
+    lcd.print("Get ready !");
+    delay(3000);
+}
+
 // Winning melody
-void win() {
+void winMelody() {
   int size = sizeof(melodyWin) / sizeof(int);
   for (int thisNote = 0; thisNote < size; thisNote++) {
     int noteDuration = 1000 / melodyWinTempo[thisNote];
@@ -217,7 +210,7 @@ void win() {
 }
 
 // Goal melody
-void goal() {
+void goalMelody() {
   int size = sizeof(melodyGoal) / sizeof(int);
   for (int thisNote = 0; thisNote < size; thisNote++) {
     int noteDuration = 1000 / melodyGoalTempo[thisNote];
